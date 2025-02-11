421edaede100c039432e153b36371ab060776c10
Konstantin <mezazoy1@yandex.ru>
2025-02-11 22:07:58 +0300

    исправлены замечания

---
+++ src/ru/yandex/test/SubtaskTest.java
@@ -0,0 +1,63 @@
+package ru.yandex.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.yandex.taskTraker.model.Epic;
+import ru.yandex.taskTraker.model.Subtask;
+import ru.yandex.taskTraker.model.Task;
+import ru.yandex.taskTraker.service.HistoryManager;
+import ru.yandex.taskTraker.service.InMemoryTaskManager;
+import ru.yandex.taskTraker.service.Managers;
+import ru.yandex.taskTraker.service.Status;
+
+import java.util.List;
+
+public class SubtaskTest {
+    InMemoryTaskManager taskManager = new InMemoryTaskManager();
+    HistoryManager historyManager = Managers.getDefaultHistory();
+
+    @Test
+    void addNewSubtask() {
+        Epic epic = new Epic("Test addNewTask", "Test addNewTask description");
+        Subtask subtask = new Subtask("test name", "test deskription", Status.NEW, epic.getId());
+
+        final int subtaskId = taskManager.add(subtask);
+
+        final Subtask savedSubtask = taskManager.getSubtaskByIdentifier(subtaskId);
+
+        Assertions.assertNotNull(savedSubtask, "Задача не найдена.");
+        Assertions.assertEquals(subtask, savedSubtask, "Задачи не совпадают.");
+
+        final List<Subtask> subtasks = taskManager.getSubtasks();
+
+        Assertions.assertNotNull(subtasks, "Задачи не возвращаются.");
+        Assertions.assertEquals(1, subtasks.size(), "Неверное количество задач.");
+        Assertions.assertEquals(subtask, subtasks.get(0), "Задачи не совпадают.");
+    }
+
+    @Test
+    void immutabilityTask() {
+        Epic epic = new Epic("Test addNewTask", "Test addNewTask description");
+        Subtask subtask = new Subtask("test name", "test deskription", Status.NEW, epic.getId());
+
+        String descr1 = subtask.getDescription();
+        String name1 = subtask.getTaskName();
+        Status status1 = subtask.getStatusTask();
+        int taskId = taskManager.add(subtask);
+        Assertions.assertEquals(name1, taskManager.getSubtaskByIdentifier(taskId).getTaskName() ,"Поле изменилось");
+        Assertions.assertEquals(status1, taskManager.getSubtaskByIdentifier(taskId).getStatusTask() ,"Поле изменилось");
+        Assertions.assertEquals(descr1, taskManager.getSubtaskByIdentifier(
+                taskId).getDescription() ,"Поле изменилось");
+    }
+
+    @Test
+    void addHistory() {
+        Epic epic = new Epic("Test addNewTask", "Test addNewTask description");
+        Subtask subtask = new Subtask("test name", "test deskription", Status.NEW, epic.getId());
+
+        historyManager.add(subtask);
+        final List<Task> history = historyManager.getHistory();
+        Assertions.assertNotNull(history, "История не пустая.");
+        Assertions.assertEquals(1, history.size(), "История не пустая.");
+    }
+}
---
+++ src/ru/yandex/test/TaskTest.java
@@ -0,0 +1,63 @@
+package ru.yandex.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.yandex.taskTraker.model.Task;
+import ru.yandex.taskTraker.service.HistoryManager;
+import ru.yandex.taskTraker.service.Managers;
+import ru.yandex.taskTraker.service.Status;
+import ru.yandex.taskTraker.service.TaskManager;
+
+import java.util.List;
+
+import static ru.yandex.taskTraker.service.Status.NEW;
+
+class TaskTest {
+    TaskManager taskManager = Managers.getDefault();
+    HistoryManager historyManager = Managers.getDefaultHistory();
+
+
+    @Test
+    void addNewTask() {
+        Task task = new Task("Test addNewTask", "Test addNewTask description", NEW);
+
+        final int taskId = taskManager.add(task);
+
+        final Task savedTask = taskManager.getTaskByIdentifier(taskId);
+
+        Assertions.assertNotNull(savedTask, "Задача не найдена.");
+        Assertions.assertEquals(task, savedTask, "Задачи не совпадают.");
+
+        final List<Task> tasks = taskManager.getTasks();
+
+        Assertions.assertNotNull(tasks, "Задачи не возвращаются.");
+        Assertions.assertEquals(1, tasks.size(), "Неверное количество задач.");
+        Assertions.assertEquals(task, tasks.get(0), "Задачи не совпадают.");
+    }
+
+    @Test
+    void addHistory() {
+        Task task = new Task("Test addNewTask", "Test addNewTask description", NEW);
+
+        historyManager.add(task);
+        final List<Task> history = historyManager.getHistory();
+        Assertions.assertNotNull(history, "История не пустая.");
+        Assertions.assertEquals(1, history.size(), "История не пустая.");
+    }
+
+    @Test
+    void immutabilityTask() {
+        Task task = new Task("Test addNewTask", "Test addNewTask description", NEW);
+
+        String descr1 = task.getDescription();
+        String name1 = task.getTaskName();
+        Status status1 = task.getStatusTask();
+        int taskId = taskManager.add(task);
+        Assertions.assertEquals(name1, taskManager.getTaskByIdentifier(taskId).getTaskName(), "Поле изменилось");
+        Assertions.assertEquals(status1, taskManager.getTaskByIdentifier(taskId).getStatusTask(), "Поле изменилось");
+        Assertions.assertEquals(descr1, taskManager.getTaskByIdentifier(
+                taskId).getDescription(), "Поле изменилось");
+    }
+
+
+}
