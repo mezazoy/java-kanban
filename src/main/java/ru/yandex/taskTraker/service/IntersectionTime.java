c060a80edf03ab3080a7fdef686c504809327454
Konstantin <mezazoy1@yandex.ru>
2025-04-17 16:04:11 +0300

    исправлен кодстайл

--- src/main/java/ru/yandex/taskTraker/service/IntersectionTime.java
+++ src/main/java/ru/yandex/taskTraker/service/IntersectionTime.java
@@ -1,7 +1,5 @@
 package ru.yandex.taskTraker.service;
 
-import java.io.IOException;
-
 public class IntersectionTime extends RuntimeException {
     public IntersectionTime(String message) {
         super(message);
